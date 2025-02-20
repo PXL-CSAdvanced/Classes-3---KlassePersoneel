fc571d5f43c79101224ce10bad0a1b8cf7c04119
Wim Bervoets <wim.bervoets@pxl.be>
2025-02-20 15:35:09 +0100

    final

---
+++ Personeel/Personeel.Shared/Employee.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personeel.Shared
+{
+    public class Employee
+    {
+        #region Variables & Properties
+
+        public string LastName { get; set; }
+
+        public string FirstName { get; set; }
+
+        private char _sex;
+
+		public char Sex
+		{
+			get { return _sex; }
+			set { _sex = value; }
+		}
+
+		private int _score;
+
+		public int Score
+		{
+			get { return _score; }
+			set 
+			{ 
+				_score = value; 
+				if(_score > 10)
+				{
+					_score = 10;
+				}
+				else if(_score < 0)
+				{
+					_score = 0;
+				}
+			}
+		}
+		
+		private int _startYear;
+
+		public int StartYear
+		{
+			get { return _startYear; }
+			set { _startYear = value; }
+		}
+
+		public int YearsOfService
+		{
+			get { return DateTime.Today.Year - StartYear; }
+		}
+
+        public string SexDescription
+        {
+            get 
+			{
+				switch(this.Sex)
+				{
+					case 'M':
+						return "Male";
+					case 'F':
+						return "Female";
+					case 'X':
+						return "Other";
+					default:
+						return "Unknown";
+				}
+			}
+        }
+
+		public decimal Bonus
+		{
+			get
+			{
+				return CalculateBonus();
+			}
+		}
+        #endregion
+
+        #region Constructors
+
+        public Employee()
+        {
+            
+        }
+
+        public Employee(string lastName, string firstName, char sex, int score, int startYear)
+        {
+            
+        }
+
+        #endregion
+
+        #region Methods & Functions
+
+        private decimal CalculateBonus()
+		{
+			decimal basis = 500 + (this.YearsOfService * 20);
+
+			if(this.Score < 5)
+			{
+				return basis / 2M;
+			}
+			else if(this.Score >= 7 && this.Score <= 8)
+			{
+				return basis * 1.5M;
+			}
+			else if(this.Score >= 9)
+			{
+				return basis * 2M;
+			}
+			else
+			{
+				return basis;
+			}
+		}
+
+		public void IncreaseScore()
+		{
+			this.Score++;
+		}
+
+		public void DecreaseScore()
+		{
+			this.Score--;
+		}
+
+		public string ShowSummary()
+		{
+			StringBuilder sb = new StringBuilder();
+			sb.AppendLine($"Personeelslid: {this.FirstName} {this.LastName}");
+			sb.AppendLine();
+			sb.AppendLine($"Geslacht:    {this.SexDescription}");
+			sb.AppendLine($"Dienstjaren: {this.YearsOfService}");
+			sb.AppendLine($"Beoordeling: {this.Score}");
+			sb.AppendLine($"Premie:      {this.Bonus}");
+
+			return sb.ToString();
+		}
+
+        #endregion
+    }
+}
---
+++ Personeel/Personeel.WPF/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Personeel.Shared;
+
+namespace Personeel.WPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    Employee employee;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        employee = new Employee();
+
+        MessageBoxResult result =
+            MessageBox.Show("Starten met ingevulde waarden?", "Start", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+        if (result == MessageBoxResult.Yes)
+        {
+            //Example data:
+            firstNameTextBox.Text = "John";
+            lastNameTextBox.Text = "Doe";
+            startYearTextBox.Text = "2010";
+            sexComboBox.SelectedIndex = 0;
+        }
+        else if (result == MessageBoxResult.No)
+        {
+            firstNameTextBox.Text = string.Empty;
+            lastNameTextBox.Text = string.Empty;
+            startYearTextBox.Text = "2000";
+            sexComboBox.SelectedIndex = 0;
+            firstNameTextBox.Focus();
+        }
+
+        ProcessInput();
+    }
+
+    private void OnIncreaseScore_Clicked(object sender, RoutedEventArgs e)
+    {
+        employee.IncreaseScore();
+        resultTextBox.Text = employee.ShowSummary();
+    }
+
+    private void OnDecreaseScore_Clicked(object sender, RoutedEventArgs e)
+    {
+        employee.DecreaseScore();
+        resultTextBox.Text = employee.ShowSummary();
+    }
+
+
+
+    private void OnInput_LostFocus(object sender, RoutedEventArgs e)
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        try
+        {
+            employee.FirstName = firstNameTextBox.Text;
+            employee.LastName = lastNameTextBox.Text;
+            if (int.TryParse(startYearTextBox.Text, out int year))
+            {
+                employee.StartYear = year;
+            }
+            if (char.TryParse(((ComboBoxItem)sexComboBox.SelectedItem).Content.ToString(), out char sex))
+            {
+                employee.Sex = sex;
+            }
+
+            resultTextBox.Text = employee.ShowSummary();
+        }
+        catch (Exception)
+        {
+            MessageBox.Show("Onverwachte fout", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
